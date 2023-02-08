210deb5c623aa641b443e131658cc5088e8158b1
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-08 11:15:42 +0000

    App calls starwars api and displays the first person

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { StarwarsPerson } from './starwars_person';
 
-function App() {
+const App : React.FC = () => {
+  const [person, setPerson] = useState<StarwarsPerson>();
+
+  useEffect(() => {
+    getPerson();
+  }, []);
+
+  const getPerson = async () => {
+    const apiResponse = await fetch(`https://swapi.dev/api/people`);
+    const json = await apiResponse.json() as { results: StarwarsPerson[] };
+    const firstPerson = json.results[0];
+    setPerson(firstPerson);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {person && <p>{person.name}</p>}
     </div>
   );
 }
